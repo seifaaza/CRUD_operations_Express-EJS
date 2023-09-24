a6bd3c41cf7c3385c991170935914d94df93e7e6
seif <seifeddine.aaza@gmail.com>
2023-09-24 21:34:06 +0100

    add static assets

--- app.js
+++ app.js
@@ -9,12 +9,14 @@ app.set('view engine', 'ejs')
 app.listen(port, _ => console.log(`App running on ${port}`))
 
 let products = [
-    { id: 1, name: 'iPhone 12 Pro', price: 1099.99, des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus omnis cumque minima, similique illum!' },
-    { id: 2, name: 'Samsung Galaxy S21', price: 999.99, des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus omnis cumque minima, similique illum!' },
-    { id: 3, name: 'Sony PlayStation 5', price: 499.99, des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus omnis cumque minima, similique illum!' },
-    { id: 4, name: 'MacBook Pro 16', price: 2399.99, des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus omnis cumque minima, similique illum!' },
-    { id: 5, name: 'DJI Mavic Air 2', price: 799.99, des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus omnis cumque minima, similique illum!' },
+    { id: 1, img: "img.jpg", name: 'iPhone 12 Pro', price: 1099.99, des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus omnis cumque minima, similique illum!' },
+    { id: 2, img: "img.jpg", name: 'Samsung Galaxy S21', price: 999.99, des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus omnis cumque minima, similique illum!' },
+    { id: 3, img: "img.jpg", name: 'Sony PlayStation 5', price: 499.99, des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus omnis cumque minima, similique illum!' },
+    { id: 4, img: "img.jpg", name: 'MacBook Pro 16', price: 2399.99, des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus omnis cumque minima, similique illum!' },
+    { id: 5, img: "img.jpg", name: 'DJI Mavic Air 2', price: 799.99, des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus omnis cumque minima, similique illum!' },
   ];
+  
+app.use(express.static('public'))
 
 app.use(middleware.loggingMiddleware)
 app.use(middleware.errorHandlingMiddleware)
@@ -38,8 +40,8 @@ app.get('/products/search/p', (req, res) => {
 })
 
 app.post('/product', (req, res) => {
-    const {name, price} = req.query
-    const newProduct = {id : products.length++, name , price: parseInt(price)}
+    const {img, name, price, des} = req.query
+    const newProduct = {id : products.length++, name, img, des , price: parseInt(price)}
     products.push(newProduct)
     res.send(products)
 })
@@ -49,6 +51,8 @@ app.put('/editProduct/:id', (req, res) => {
     const { name, price } = req.query;
     const itemIdx = products.findIndex(product => product.id === parseInt(id));
     name ? products[itemIdx].name = name : name
+    img ? products[itemIdx].img = img : img
+    des ? products[itemIdx].des = des : des
     price ? products[itemIdx].price = parseInt(price) : price
     res.send(products[itemIdx]);
 });
