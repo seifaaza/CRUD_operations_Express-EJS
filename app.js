d3b8b33857f4a0b384ba10f4c4499eec11869185
seif <seifeddine.aaza@gmail.com>
2023-09-24 20:56:16 +0100

    ejs

--- app.js
+++ app.js
@@ -5,25 +5,29 @@ const middleware = require('./middleware')
 
 const port = 3000
 
+app.set('view engine', 'ejs')
 app.listen(port, _ => console.log(`App running on ${port}`))
 
 let products = [
-    { id: 1, name: 'iPhone 12 Pro', price: 1099.99 },
-    { id: 2, name: 'Samsung Galaxy S21', price: 999.99 },
-    { id: 3, name: 'Sony PlayStation 5', price: 499.99 },
-    { id: 4, name: 'MacBook Pro 16', price: 2399.99 },
-    { id: 5, name: 'DJI Mavic Air 2', price: 799.99 },
+    { id: 1, name: 'iPhone 12 Pro', price: 1099.99, des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus omnis cumque minima, similique illum!' },
+    { id: 2, name: 'Samsung Galaxy S21', price: 999.99, des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus omnis cumque minima, similique illum!' },
+    { id: 3, name: 'Sony PlayStation 5', price: 499.99, des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus omnis cumque minima, similique illum!' },
+    { id: 4, name: 'MacBook Pro 16', price: 2399.99, des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus omnis cumque minima, similique illum!' },
+    { id: 5, name: 'DJI Mavic Air 2', price: 799.99, des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus omnis cumque minima, similique illum!' },
   ];
 
 app.use(middleware.loggingMiddleware)
 app.use(middleware.errorHandlingMiddleware)
 
-app.get('/products', (req, res) => res.send(products).status(403))
+app.get('/', (req, res) => { 
+    res.render('home', {products})
+})
 
-app.get('/products/:id', (req, res) => {
+app.get('/productDetails/:id', (req, res) => {
     const id = req.params.id
-    const product = products.filter(product => product.id === parseInt(id) ? product : null)
-    res.send(product)
+    const itemIdx = products.findIndex(product => product.id === parseInt(id));
+    res.render('productDetails', {productItem: products[itemIdx]})
+        
 })
 
 app.get('/products/search/p', (req, res) => {
@@ -33,14 +37,14 @@ app.get('/products/search/p', (req, res) => {
     res.send(product)
 })
 
-app.post('/products', (req, res) => {
+app.post('/product', (req, res) => {
     const {name, price} = req.query
     const newProduct = {id : products.length++, name , price: parseInt(price)}
     products.push(newProduct)
     res.send(products)
 })
 
-app.put('/products/:id', (req, res) => {
+app.put('/editProduct/:id', (req, res) => {
     const id = req.params.id;
     const { name, price } = req.query;
     const itemIdx = products.findIndex(product => product.id === parseInt(id));
@@ -49,7 +53,7 @@ app.put('/products/:id', (req, res) => {
     res.send(products[itemIdx]);
 });
 
-app.delete('/products/:id', (req, res) => {
+app.delete('/deleteProduct/:id', (req, res) => {
     const id = req.params.id;
     const itemIdx = products.findIndex(product => product.id === parseInt(id));
     products.splice(itemIdx, 1);
